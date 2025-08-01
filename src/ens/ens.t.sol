2d467c66b974751c739c91d6b337877bc7ba8756
Alexandro T. Netto <alex.t.netto@gmail.com>
2025-08-01 01:34:57 -0300

    feat: calldata json comparison inherted

--- src/ens/ens.t.sol
+++ src/ens/ens.t.sol
@@ -174,6 +174,10 @@ abstract contract ENS_Governance is Test, IDAO, ENSHelper {
 
         // Assert parameters modified after execution
         _afterExecution();
+
+        if (keccak256(abi.encodePacked(jsonPath())) != keccak256(abi.encodePacked(""))) {
+            draftCallDataComparison();
+        }
     }
 
     function _selectFork() public virtual {
@@ -214,4 +218,130 @@ abstract contract ENS_Governance is Test, IDAO, ENSHelper {
     function _beforeProposal() public virtual;
 
     function _afterExecution() public virtual;
+
+    function jsonPath() public virtual returns (string memory) {
+        return "";
+    }
+
+    function draftCallDataComparison() public {
+        string memory jsonContent = vm.readFile(jsonPath());
+    
+        address[] memory jsonTargets = parseJsonTargets(jsonContent);
+        string[] memory jsonValues = parseJsonValues(jsonContent);
+        bytes[] memory jsonCalldatas = parseJsonCalldatas(jsonContent);
+
+        console2.log("JSON parsed successfully with", jsonTargets.length, "operations");
+
+        // Generate calldata from the contract
+        (
+            address[] memory generatedTargets,
+            uint256[] memory generatedValues,
+            string[] memory generatedSignatures,
+            bytes[] memory generatedCalldatas,
+            string memory generatedDescription
+        ) = _generateCallData();
+        
+        // Compare lengths
+        assertEq(
+            jsonTargets.length,
+            generatedTargets.length,
+            "Number of executable calls mismatch"
+        );
+        
+        // Compare each operation
+        for (uint256 i = 0; i < jsonTargets.length; i++) {
+            // Compare target addresses
+            assertEq(
+                jsonTargets[i],
+                generatedTargets[i],
+                string(abi.encodePacked("Target mismatch at index ", vm.toString(i)))
+            );
+            
+            // Compare values
+            assertEq(
+                vm.parseUint(jsonValues[i]),
+                generatedValues[i],
+                string(abi.encodePacked("Value mismatch at index ", vm.toString(i)))
+            );
+            
+            // Compare calldata
+            assertEq(
+                jsonCalldatas[i],
+                generatedCalldatas[i],
+                string(abi.encodePacked("Calldata mismatch at index ", vm.toString(i)))
+            );
+        }
+
+        assertEq(jsonTargets.length, jsonValues.length, "Targets and values arrays length mismatch");
+        assertEq(jsonTargets.length, jsonCalldatas.length, "Targets and calldata arrays length mismatch");
+    }
+
+    function decodeTargetsArray(string memory jsonContent) public returns (address[] memory) {
+        return abi.decode(vm.parseJson(jsonContent, ".executableCalls[*].target"), (address[]));
+    }
+
+    function decodeTargetSingle(string memory jsonContent) public returns (address) {
+        return abi.decode(vm.parseJson(jsonContent, ".executableCalls[*].target"), (address));
+    }
+
+    function parseJsonTargets(string memory jsonContent) public returns (address[] memory jsonTargets) {
+        bytes memory data = abi.encodeWithSelector(this.decodeTargetsArray.selector, jsonContent);
+        (bool success, bytes memory returnData) = address(this).call(data);
+
+        if (success) {
+            jsonTargets = abi.decode(returnData, (address[]));
+        } else {
+            bytes memory singleData = abi.encodeWithSelector(this.decodeTargetSingle.selector, jsonContent);
+            (bool ok, bytes memory ret) = address(this).call(singleData);
+            require(ok, "Single decode failed");
+            jsonTargets = new address[](1);
+            jsonTargets[0] = abi.decode(ret, (address));
+        }
+    }
+
+    function decodeValuesArray(string memory jsonContent) public returns (string[] memory) {
+        return abi.decode(vm.parseJson(jsonContent, ".executableCalls[*].value"), (string[]));
+    }
+
+    function decodeValueSingle(string memory jsonContent) public returns (string memory) {
+        return abi.decode(vm.parseJson(jsonContent, ".executableCalls[*].value"), (string));
+    }   
+    
+    function parseJsonValues(string memory jsonContent) public returns (string[] memory jsonValues) {
+        bytes memory data = abi.encodeWithSelector(this.decodeValuesArray.selector, jsonContent);
+        (bool success, bytes memory returnData) = address(this).call(data);
+
+        if (success) {
+            jsonValues = abi.decode(returnData, (string[]));
+        } else {
+            bytes memory singleData = abi.encodeWithSelector(this.decodeValueSingle.selector, jsonContent);
+            (bool ok, bytes memory ret) = address(this).call(singleData);
+            require(ok, "Single decode failed");
+            jsonValues = new string[](1);
+            jsonValues[0] = abi.decode(ret, (string));
+        }
+    }
+
+    function decodeCalldatasArray(string memory jsonContent) public returns (bytes[] memory) {
+        return abi.decode(vm.parseJson(jsonContent, ".executableCalls[*].calldata"), (bytes[]));
+    }
+
+    function decodeCalldataSingle(string memory jsonContent) public returns (bytes memory) {
+        return abi.decode(vm.parseJson(jsonContent, ".executableCalls[*].calldata"), (bytes));
+    }
+    
+    function parseJsonCalldatas(string memory jsonContent) public returns (bytes[] memory jsonCalldatas) {
+        bytes memory data = abi.encodeWithSelector(this.decodeCalldatasArray.selector, jsonContent);
+        (bool success, bytes memory returnData) = address(this).call(data);
+
+        if (success) {
+            jsonCalldatas = abi.decode(returnData, (bytes[]));
+        } else {
+            bytes memory singleData = abi.encodeWithSelector(this.decodeCalldataSingle.selector, jsonContent);
+            (bool ok, bytes memory ret) = address(this).call(singleData);
+            require(ok, "Single decode failed");
+            jsonCalldatas = new bytes[](1);
+            jsonCalldatas[0] = abi.decode(ret, (bytes));
+        }
+    }
 }
