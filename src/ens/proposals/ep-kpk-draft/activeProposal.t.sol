ddd0cf2a99270d0d90cd1297e9171431f8816ea6
Alexandro T. Netto <alex.t.netto@gmail.com>
2025-12-04 15:53:15 -0300

    feat: IZodiacRoles imported

--- src/ens/proposals/ep-kpk-draft/activeProposal.t.sol
+++ src/ens/proposals/ep-kpk-draft/activeProposal.t.sol
@@ -2,67 +2,10 @@
 pragma solidity ^0.8.25;
 
 import { ISafe } from "@ens/interfaces/ISafe.sol";
+import { IZodiacRoles } from "@ens/interfaces/IZodiacRoles.sol";
 import { ENS_Governance } from "@ens/ens.t.sol";
 import { console2 } from "@forge-std/src/console2.sol";
 import { IERC20 } from "@forge-std/src/interfaces/IERC20.sol";
-
-interface IZodiacRoles {
-    enum Status {
-        Ok,
-        /// Role not allowed to delegate call to target address
-        DelegateCallNotAllowed,
-        /// Role not allowed to call target address
-        TargetAddressNotAllowed,
-        /// Role not allowed to call this function on target address
-        FunctionNotAllowed,
-        /// Role not allowed to send to target address
-        SendNotAllowed,
-        /// Or conition not met
-        OrViolation,
-        /// Nor conition not met
-        NorViolation,
-        /// Parameter value is not equal to allowed
-        ParameterNotAllowed,
-        /// Parameter value less than allowed
-        ParameterLessThanAllowed,
-        /// Parameter value greater than maximum allowed by role
-        ParameterGreaterThanAllowed,
-        /// Parameter value does not match
-        ParameterNotAMatch,
-        /// Array elements do not meet allowed criteria for every element
-        NotEveryArrayElementPasses,
-        /// Array elements do not meet allowed criteria for at least one element
-        NoArrayElementPasses,
-        /// Parameter value not a subset of allowed
-        ParameterNotSubsetOfAllowed,
-        /// Bitmask exceeded value length
-        BitmaskOverflow,
-        /// Bitmask not an allowed value
-        BitmaskNotAllowed,
-        CustomConditionViolation,
-        AllowanceExceeded,
-        CallAllowanceExceeded,
-        EtherAllowanceExceeded
-    }
-
-    error ConditionViolation(Status, bytes32);
-
-    enum Operation {
-        Call,
-        DelegateCall
-    }
-
-    function execTransactionWithRole(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        Operation operation,
-        bytes32 roleKey,
-        bool shouldRevert
-    )
-        external
-        returns (bool success);
-}
 
 contract Proposal_ENS_EP_KPK_DRAFT_Test is ENS_Governance {
     address private safe = 0x4F2083f5fBede34C2714aFfb3105539775f7FE64;
