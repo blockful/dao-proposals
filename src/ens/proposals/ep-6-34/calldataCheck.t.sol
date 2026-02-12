3fd7ac277dc0b5eebe58d5df621720dd2ba9c076
Alexandro T. Netto <alex.t.netto@gmail.com>
2026-02-12 10:38:42 -0300

    test(ens): validate chain resolver in ep-6-34

--- src/ens/proposals/ep-6-34/calldataCheck.t.sol
+++ src/ens/proposals/ep-6-34/calldataCheck.t.sol
@@ -5,12 +5,19 @@ import { ENS_Governance } from "@ens/ens.t.sol";
 import { IENSRegistrar } from "@ens/interfaces/IENSRegistrar.sol";
 import { IENSRegistryWithFallback } from "@ens/interfaces/IENSRegistryWithFallback.sol";
 
+interface IChainResolver {
+    function owner() external view returns (address);
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
 contract Proposal_ENS_EP_6_34_Draft_Test is ENS_Governance {
     IENSRegistrar public constant baseRegistrar = IENSRegistrar(0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85);
     IENSRegistryWithFallback public constant ensRegistry =
         IENSRegistryWithFallback(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
 
     address public constant CHAIN_RESOLVER = 0x2a9B5787207863cf2d63d20172ed1F7bB2c9487A;
+    address public constant CHAIN_RESOLVER_OWNER = 0x81c11034FE2b2F0561e9975Df9a45D99172183Af;
+    bytes4 public constant IERC165_INTERFACE_ID = 0x01ffc9a7;
 
     bytes32 public constant ON_LABELHASH = 0x6460d40e0362f6a2c743f205df8181010b7f26e76d5606847fb7be7fb6d135f9;
     bytes32 public constant ON_ETH_NODE = 0xcabf8262fe531c2a7e8cd86e06342bc27fc0591ecd562fbac88280abc18ef899;
@@ -116,11 +123,16 @@ contract Proposal_ENS_EP_6_34_Draft_Test is ENS_Governance {
         address onNodeOwnerAfter = ensRegistry.owner(ON_ETH_NODE);
         address onResolverAfter = ensRegistry.resolver(ON_ETH_NODE);
         uint256 onExpiryAfter = baseRegistrar.nameExpires(ON_TOKEN_ID);
+        address chainResolverOwnerAfter = IChainResolver(CHAIN_RESOLVER).owner();
+        bool chainResolverSupportsERC165 = IChainResolver(CHAIN_RESOLVER).supportsInterface(IERC165_INTERFACE_ID);
 
         assertFalse(timelockControllerAfter, "Timelock controller should be removed after execution");
         assertEq(onTokenOwnerAfter, address(timelock), "on.eth NFT owner should be timelock");
         assertEq(onNodeOwnerAfter, address(timelock), "on.eth ENS node owner should be timelock");
         assertEq(onResolverAfter, CHAIN_RESOLVER, "on.eth resolver should be ChainResolver");
+        assertGt(CHAIN_RESOLVER.code.length, 0, "ChainResolver should be a deployed contract");
+        assertEq(chainResolverOwnerAfter, CHAIN_RESOLVER_OWNER, "Unexpected ChainResolver owner");
+        assertTrue(chainResolverSupportsERC165, "ChainResolver should support ERC165");
         assertEq(onExpiryAfter, block.timestamp + REGISTRATION_DURATION, "on.eth expiry should be extended by 10 years");
         assertGt(onExpiryAfter, onExpiryBefore, "on.eth expiry should increase");
     }
