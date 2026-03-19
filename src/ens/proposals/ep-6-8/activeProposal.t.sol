a096cabe27475ba2873dde1d6f4a2c1f87a6e408
Alexandro T. Netto <alex.t.netto@gmail.com>
2026-03-19 00:46:33 -0300

    refactor(ens): replace local IZodiacRoles with shared interface in EP 6.8 and EP 6.23
    Remove ~116 lines of duplicated IZodiacRoles interface definitions
    from ep-6-8 and ep-6-23 proposal tests, replacing with imports from
    the shared @ens/interfaces/IZodiacRoles.sol.

--- src/ens/proposals/ep-6-8/activeProposal.t.sol
+++ src/ens/proposals/ep-6-8/activeProposal.t.sol
@@ -11,6 +11,7 @@ import { ISafe } from "@ens/interfaces/ISafe.sol";
 import { IERC20 } from "@contracts/utils/interfaces/IERC20.sol";
 
 import { ENS_Governance } from "@ens/ens.t.sol";
+import { IZodiacRoles } from "@ens/interfaces/IZodiacRoles.sol";
 
 interface AaveV3 {
     function depositETH(address pool, address onBehalfOf, uint16 referralCode) external payable;
@@ -149,64 +150,6 @@ interface IOETH {
 interface Origin {
     function requestWithdrawal(uint256) external;
     function claimWithdrawal(uint256) external;
-}
-
-interface IZodiacRoles {
-    enum Status {
-        Ok,
-        /// Role not allowed to delegate call to target address
-        DelegateCallNotAllowed,
-        /// Role not allowed to call target address
-        TargetAddressNotAllowed,
-        /// Role not allowed to call this function on target address
-        FunctionNotAllowed,
-        /// Role not allowed to send to target address
-        SendNotAllowed,
-        /// Or conition not met
-        OrViolation,
-        /// Nor conition not met
-        NorViolation,
-        /// Parameter value is not equal to allowed
-        ParameterNotAllowed,
-        /// Parameter value less than allowed
-        ParameterLessThanAllowed,
-        /// Parameter value greater than maximum allowed by role
-        ParameterGreaterThanAllowed,
-        /// Parameter value does not match
-        ParameterNotAMatch,
-        /// Array elements do not meet allowed criteria for every element
-        NotEveryArrayElementPasses,
-        /// Array elements do not meet allowed criteria for at least one element
-        NoArrayElementPasses,
-        /// Parameter value not a subset of allowed
-        ParameterNotSubsetOfAllowed,
-        /// Bitmask exceeded value length
-        BitmaskOverflow,
-        /// Bitmask not an allowed value
-        BitmaskNotAllowed,
-        CustomConditionViolation,
-        AllowanceExceeded,
-        CallAllowanceExceeded,
-        EtherAllowanceExceeded
-    }
-
-    error ConditionViolation(Status, bytes32);
-
-    enum Operation {
-        Call,
-        DelegateCall
-    }
-
-    function execTransactionWithRole(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        Operation operation,
-        bytes32 roleKey,
-        bool shouldRevert
-    )
-        external
-        returns (bool success);
 }
 
 contract Proposal_ENS_EP_6_8_Test is ENS_Governance {
